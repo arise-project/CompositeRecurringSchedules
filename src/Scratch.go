c9345e9c6ebe0b69563dba9a0df8f0cb7a22b572
Evhen Pyrohov <epirogov@windowslive.com>
2016-01-09 19:57:20 +0000

    scheduler script correction

--- src/Scratch.go
+++ src/Scratch.go
@@ -9,8 +9,24 @@ func main() {
     var str = "{every} {1} {dec 25} {starting dec 25 2016} {do nothing} = $christmas {every} {2} {weeks} {starting jan 1 2016} {send email to list} EXCEPT {$Christmas} = biweeklynewsletter"
     fmt.Println("Example : ")
     fmt.Printf("%v\n\n",str)
+    fmt.Println("Initial parsing : ")
     var words = strings.SplitAfterN(str, "}", strings.Count(str,"{"))
     fmt.Printf("%q\n", words)
+    
+    
+    fmt.Println("Correction : ")
+    for index,element := range words {
+        // index is the index where we are
+        // element is the element from someSlice for where we are
+        if index > 0 {
+            if strings.Index(element, "{") > 1 {
+                words[index - 1] = words[index - 1] +  element[0:strings.Index(element, "{") - 1]
+                words[index] = element[strings.Index(element, "{"):len(element)]
+            }
+        }
+    }
+    fmt.Printf("%q\n", words)
+    
 }
 
 
